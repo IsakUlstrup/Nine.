804c39368d982197fda329d4e4b1208b3b268e59
Isak <isakulstrup@gmail.com>
2019-12-16 15:16:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+
+// Grab elements, create settings, etc.
+var constraints = {
+  video: {
+    width: 1000,
+    height: 1000,
+    facingMode: { ideal: 'environment' }
+  } 
+}
+// var xhr = new XMLHttpRequest()
+var canvas = document.getElementById('canvas')
+var context = canvas.getContext('2d')
+var video = document.getElementById('video')
+var nineData = []
+// const serverURL = 'https://anna.local'
+
+// camera access
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(stream) {
+  video.srcObject = stream;
+  video.play()
+})
+.catch(function(err) {
+  console.log(err)
+})
+
+function getColors () {
+  // loop canvas, and get rgb values for each pixel
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+      var pixelData = context.getImageData(x, y, 1, 1).data
+      pixelData = {
+        r: pixelData[0],
+        g: pixelData[1],
+        b: pixelData[2]
+      }
+      // set background color of corresponding grid element
+      nineData.push(pixelData)
+      document.getElementById(x.toString() + y.toString()).style.backgroundColor = `rgb(${pixelData.r}, ${pixelData.g}, ${pixelData.b})`
+    }
+  }
+}
+
+// Trigger photo take
+function takePhoto() {
+  // draw current frame from video feed to canvas, in 3x3 resolution
+  context.drawImage(video, 0, 0, 3, 3)
+  nineData = []
+}
+
+function update() {
+  takePhoto()
+  getColors()
+}
+
+setInterval(update, 100)
